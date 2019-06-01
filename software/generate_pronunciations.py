abc9aac85cba66c3eef04b2233fc33aa70f7b395
nurbu5 <dawansherpa5@gmail.com>
2019-05-31 23:45:39 -0600

    Fix loop typo

--- software/generate_pronunciations.py
+++ software/generate_pronunciations.py
@@ -4,7 +4,7 @@ PRONUNCIATIONS_DIRECTORY = "../data/pronunciations/"
 
 phrase = input('Enter the phrase you\'d like pronounced (or "exit" to quit): ')
 
-for phrase != 'exit':
+while phrase != 'exit':
     tts = gTTS(phrase, lang="zh-cn") #or zh-tw for taiwanese pronunciation
     tts.save(PRONUNCIATIONS_DIRECTORY + phrase + '.mp3')
     phrase = input('Enter the phrase you\'d like pronounced (or "exit" to quit): ')
