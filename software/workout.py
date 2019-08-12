0f20cb4ad5ae8001edadf2219430d9125ba470f0
nurbu5 <dawansherpa5@gmail.com>
2019-08-12 07:50:25 -0600

    Update workout.py

--- software/workout.py
+++ software/workout.py
@@ -3,10 +3,12 @@ import time
 
 PRONUNCIATIONS_DIRECTORY = "../data/pronunciations/"
 OS_PLAY_COMMAND = "afplay "
+EXERCISE_DURATION = 35
 
 exercises={
-    "Gymnastic Ring":"体操环",
+    #"Gymnastic Ring":"体操环",
     #"Jumping Jacks":"开合跳",
+    "Leg Lifts":"抬腿",
     "Wall Sits":"贴墙半蹲",
     "Push-ups":"伏地挺身",
     "Sit-ups":"仰卧起坐",
@@ -15,7 +17,7 @@ exercises={
     "Double arm flexion (Tricep Dips)":"双杠臂屈伸",
     "Plank":"平板支撑",
     #"High Knees":"高抬腿运动",
-    "Handstand":"手倒立",
+    #"Handstand":"手倒立",
     "Hollow Hold":"空心的",
     "Lunges":"弓步",
     "Push-ups with rotation":"伏地挺身+上半身轉體",
@@ -25,6 +27,9 @@ exercises={
 lr_exercises={
     "Side Bridge":"侧桥"
 }
+
+os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "伸展.mp3")
+input("伸展 ")
 
 os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "多少.mp3")
 circuits = input("多少 ")
@@ -37,9 +42,9 @@ for _ in range(int(circuits)):
         print("English:", en)
         print("\n")
         time.sleep(9)
-        os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "倒数.mp3") #countdown.mp3
+        os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "三二一.mp3") #countdown.mp3
         os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "开始.mp3")
-        time.sleep(30)
+        time.sleep(EXERCISE_DURATION)
         os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "停止.mp3")
 
     for en, zh in lr_exercises.items():
@@ -49,11 +54,11 @@ for _ in range(int(circuits)):
         print("English: ", en)
         print("\n")
         time.sleep(9)
-        os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "倒数.mp3") #countdown.mp3
+        os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "三二一.mp3") #countdown.mp3
         os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "开始.mp3")
-        time.sleep(20)
+        time.sleep((EXERCISE_DURATION * 2)/3)
         os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "替换.mp3")
-        time.sleep(21)
+        time.sleep(((EXERCISE_DURATION * 2)/3) + 1)
         os.system(OS_PLAY_COMMAND + PRONUNCIATIONS_DIRECTORY + "停止.mp3")
 
 
