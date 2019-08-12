79bc9663132cbd39d5764436a932058ad364e7e2
nurbu5 <dawansherpa5@gmail.com>
2019-08-12 07:46:31 -0600

    Update generate_pronunciations.py

--- software/generate_pronunciations.py
+++ software/generate_pronunciations.py
@@ -1,10 +1,19 @@
 from gtts import gTTS
 
-PRONUNCIATIONS_DIRECTORY = "../data/pronunciations/"
+ANKI_MEDIA_DIRECTORY = '/Users/dawasherpa/Library/Application Support/Anki2/User 1/collection.media/'
+#ANKI_MEDIA_DIRECTORY = "./tempDir/" #Temporary workaround
 
 phrase = input('Enter the phrase you\'d like pronounced (or "exit" to quit): ')
 
 while phrase != 'exit':
     tts = gTTS(phrase, lang="zh-cn") #or zh-tw for taiwanese pronunciation
-    tts.save(PRONUNCIATIONS_DIRECTORY + phrase + '.mp3')
+    tts.save(ANKI_MEDIA_DIRECTORY + phrase + '.mp3')
     phrase = input('Enter the phrase you\'d like pronounced (or "exit" to quit): ')
+
+#CARD_LIST_FILE = "./anki_card_list.txt"
+
+#with open(CARD_LIST_FILE) as f:
+#    for line in f:
+#        phrase = line.strip()
+#        tts = gTTS(phrase, lang="zh-cn") #or zh-tw for taiwanese pronunciation
+#        tts.save(ANKI_MEDIA_DIRECTORY + phrase + '.mp3')
